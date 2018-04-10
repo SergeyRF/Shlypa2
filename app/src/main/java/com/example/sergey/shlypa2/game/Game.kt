af90d95dee47abd8f51d943190e415aa36dcbf7b
volgoak <volgoak@gmail.com>
2018-04-10 14:24:58 +0300

    Created game state

--- app/src/main/java/com/example/sergey/shlypa2/game/Game.kt
+++ app/src/main/java/com/example/sergey/shlypa2/game/Game.kt
@@ -7,89 +7,76 @@ import java.util.*
  * Created by sergey on 3/29/18.
  */
 object Game {
-    private val players = mutableMapOf<String, Player>()
 
-    private val rounds = listOf(RoundDescriptor("First round", "Simple rules"),
-            RoundDescriptor("Second round", "Simple rules"),
-            RoundDescriptor("Third round", "Guess word or fuck yourself"))
-
-    private val teams = mutableListOf<Team>()
-    private var currentTeamPosition = 0
-
-    private var currentRound: Round? = null
-    private var currentRoundPosition = -1
-
-    private val allWords = mutableListOf<Word>()
+    var state : GameState = GameState()
     var time = 60
     var words = 5
 
+    var currentRound: Round? = null
 
-    fun maxTeamsCount(): Int = players.size / 2
+    fun maxTeamsCount(): Int = state.players.size / 2
 
 
     fun addPlayer(player: Player): Boolean {
-        return if (players.containsKey(player.name)) {
+        return if (state.players.containsKey(player.name)) {
             false
         } else {
-            players[player.name] = player
+            state.players[player.name] = player
             true
         }
     }
 
     fun removePlayer(player: Player) {
-        players.remove(player.name)
+        state.players.remove(player.name)
     }
 
-    fun getPlayers(): List<Player> = players.values.toList()
+    fun getPlayers(): List<Player> {
+        return state.players.values.toList()
+    }
 
     fun createTeams(count: Int) {
-        val shuffledPlayers: MutableList<Player> = players.values.toMutableList()
+        val shuffledPlayers: MutableList<Player> = state.players.values.toMutableList()
         Collections.shuffle(shuffledPlayers)
 
         val playersQueue = ArrayDeque<Player>()
         playersQueue.addAll(shuffledPlayers)
 
         // why not?
-        Game.teams.clear()
+        state.teams.clear()
 
         for (i in 0 until count) {
-            teams.add(Team("Team $i"))
+            state.teams.add(Team("Team $i"))
         }
 
         var currentTeam = 0
         while (playersQueue.isNotEmpty()) {
-            teams[currentTeam].players.add(playersQueue.remove())
+            state.teams[currentTeam].players.add(playersQueue.remove())
 
-            currentTeam = if (currentTeam >= teams.size - 1) 0 else currentTeam + 1
+            currentTeam = if (currentTeam >= state.teams.size - 1) 0 else currentTeam + 1
         }
     }
 
-    fun getTeams(): List<Team> = teams
+    fun getTeams(): List<Team> = state.teams
 
-    fun getCurrentTeam(): Team = teams[currentTeamPosition]
+    fun getCurrentTeam(): Team = state.teams[state.currentTeamPosition]
 
     fun nextTeam(): Team {
-        currentTeamPosition++
-        if (currentTeamPosition >= teams.size) currentTeamPosition = 0
-        return teams[currentTeamPosition]
+        state.currentTeamPosition++
+        if (state.currentTeamPosition >= state.teams.size) state.currentTeamPosition = 0
+        return state.teams[state.currentTeamPosition]
     }
 
-    fun addWord(word: Word) = allWords.add(word)
+    fun addWord(word: Word) = state.allWords.add(word)
 
-    fun getWords(): List<Word> = allWords
+    fun getWords(): List<Word> = state.allWords
 
     fun getRound(): Round = currentRound!!
 
     fun hasRound() = currentRound != null
 
     fun beginNextRound() {
-        currentRoundPosition++
-        Timber.d("current round $currentRoundPosition rounds ${rounds.size}")
-        if (currentRoundPosition < rounds.size) {
-            currentRound = Round(allWords)
-            currentRound?.description = rounds[currentRoundPosition].description
-            currentRound?.rules = rounds[currentRoundPosition].rules
-        }
+        state.currentRoundPosition++
+        currentRound = state.createRound()
     }
 
     fun finishRound() {
@@ -98,8 +85,6 @@ object Game {
     }
 
     fun clear() {
-        allWords.clear()
-        teams.clear()
-        players.clear()
+        state = GameState()
     }
 }
