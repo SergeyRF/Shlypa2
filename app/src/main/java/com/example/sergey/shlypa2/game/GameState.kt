c23ce5f79c3f2c07aa6b3009adeaf2f4dd89df55
sergey <baskervilei08@gmail.com>
2018-04-18 16:28:57 +0300

    add rules

--- app/src/main/java/com/example/sergey/shlypa2/game/GameState.kt
+++ app/src/main/java/com/example/sergey/shlypa2/game/GameState.kt
@@ -7,9 +7,17 @@ import com.example.sergey.shlypa2.beans.Word
  * Created by alex on 4/10/18.
  */
 class GameState {
-    var rounds = listOf(RoundDescriptor("First round", "Simple rules"),
-            RoundDescriptor("Second round", "Simple rules"),
-            RoundDescriptor("Third round", "Guess word or fuck yourself"))
+    var rounds = listOf(RoundDescriptor("First round", "Слово - Словами\n" +
+            "Необходимо объяснить" +
+            " своей команде как можно больше слов за отведенное время. Нельзя иcпользовать " +
+            "однокоренные, похожие по звучанию слова, использовать иностранные слова, перевод которых " +
+            "означает загаданное слово. Игра продолжается пока в шляпе не останется слов."),
+            RoundDescriptor("Second round", "Слово - Жестами\n" +
+                    "Слова объясняются жестами как в игре Крокодил. Нельзя ничего произносить вслух," +
+                    " использовать предметы, использовать других игроков, показывать на что-либо."),
+            RoundDescriptor("Third round", "Слово - Словом\n" +
+                    "Необходимо придумать к слову ассоциацию и произнести одно слово обозначающее" +
+                    " её. Запрещаются любые жесты и звуки."))
 
     val resultsList: MutableList<RoundResults> = mutableListOf()
 
