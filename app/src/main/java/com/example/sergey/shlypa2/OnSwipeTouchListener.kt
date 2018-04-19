f727779db437073893a3f95e06340acba1ed36d8
volgoak <volgoak@gmail.com>
2018-04-19 14:40:59 +0300

    Swipe for game activity

--- app/src/main/java/com/example/sergey/shlypa2/OnSwipeTouchListener.kt
+++ app/src/main/java/com/example/sergey/shlypa2/OnSwipeTouchListener.kt
@@ -4,10 +4,10 @@ package com.example.sergey.shlypa2
  * Created by sergey on 4/18/18.
  */
 
-import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import timber.log.Timber
 
 /**
  * Created by alex on 1/14/18.
@@ -17,21 +17,34 @@ open class OnSwipeTouchListener : View.OnTouchListener {
 
     private val gestureDetector = GestureDetector(GestureListener())
 
+    var scrollListener : ((distX : Float, distY : Float) -> Unit)? = null
+
+    var swipeTressHold = 100
+    var swipeVelocityTressHold = 50
+
     override fun onTouch(v: View, event: MotionEvent): Boolean {
-        return gestureDetector.onTouchEvent(event)
+
+        if(gestureDetector.onTouchEvent(event)) {
+            return true
+        } else if(event.action == MotionEvent.ACTION_UP) {
+            onActionUp()
+            return true
+        } else return false
     }
+
+
 
     private inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
 
 
         override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
-            Log.d(TAG, "onFling: ")
             var result = false
+            onActionUp()
             try {
                 val diffY = e2.y - e1.y
                 val diffX = e2.x - e1.x
                 if (Math.abs(diffX) > Math.abs(diffY)) {
-                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (Math.abs(diffX) > swipeTressHold && Math.abs(velocityX) > swipeVelocityTressHold) {
                         if (diffX > 0) {
                             result = onSwipeRight()
                         } else {
@@ -39,7 +52,7 @@ open class OnSwipeTouchListener : View.OnTouchListener {
                         }
                     }
                 } else {
-                    if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (Math.abs(diffY) > swipeTressHold && Math.abs(velocityY) > swipeVelocityTressHold) {
                         if (diffY > 0) {
                             result = onSwipeBottom()
                         } else {
@@ -54,33 +67,33 @@ open class OnSwipeTouchListener : View.OnTouchListener {
             return result
         }
 
+        override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
+            Timber.d("on scroll x dist $distanceX y dist $distanceY")
+            scrollListener?.invoke(distanceX, distanceY)
+            return super.onScroll(e1, e2, distanceX, distanceY)
+        }
+
 
     }
 
    open fun onSwipeRight(): Boolean {
-        Log.d(TAG, "onSwipeRight: ")
         return false
     }
 
     open fun onSwipeLeft(): Boolean {
-        Log.d(TAG, "onSwipeLeft: ")
         return false
     }
 
-    fun onSwipeTop(): Boolean {
-        Log.d(TAG, "onSwipeTop: ")
+    open fun onActionUp() {
+        Timber.d("onActionUp")
+    }
+
+    open fun onSwipeTop(): Boolean {
         return false
     }
 
-    fun onSwipeBottom(): Boolean {
-        Log.d(TAG, "onSwipeBottom: ")
+    open fun onSwipeBottom(): Boolean {
         return false
-    }
-
-    companion object {
-        const val SWIPE_THRESHOLD = 100
-        const val SWIPE_VELOCITY_THRESHOLD = 100
-        val TAG = OnSwipeTouchListener::class.java.simpleName
     }
 
 }
