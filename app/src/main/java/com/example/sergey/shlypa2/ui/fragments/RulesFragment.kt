61d5f4538fccb77ed448fa1dd27842b06a0f396f
volgoak <volgoak@gmail.com>
2018-05-09 14:26:42 +0300

    First release

--- app/src/main/java/com/example/sergey/shlypa2/ui/fragments/RulesFragment.kt
+++ app/src/main/java/com/example/sergey/shlypa2/ui/fragments/RulesFragment.kt
@@ -1,13 +1,18 @@
 package com.example.sergey.shlypa2.ui.fragments
 
 
+import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.sergey.shlypa2.BuildConfig
 import com.example.sergey.shlypa2.R
 import kotlinx.android.synthetic.main.fragment_rules.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.util.*
 
 
@@ -30,9 +35,29 @@ class RulesFragment : Fragment() {
 
     fun loadRules() {
         val locale: String = Locale.getDefault().language.toLowerCase()
-        when (locale) {
-            "ru" -> web_rules.loadUrl("file:///android_asset/rules.html")
-            else -> web_rules.loadUrl("file:///android_asset/rulesen.html")
+        val fileName =
+                when (locale) {
+                    "ru" -> "rules.html"
+                    else -> "rulesen.html"
+                }
+
+        val rulesStream = context!!.assets!!.open(fileName)
+        val bufferedReader = BufferedReader(InputStreamReader(rulesStream))
+
+        val stringBuilder = StringBuilder()
+        var line: String? = bufferedReader.readLine()
+
+        while (line != null) {
+            stringBuilder.append(line)
+            line = bufferedReader.readLine()
+        }
+
+        val html = stringBuilder.toString()
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            tvRules.text = Html.fromHtml(html, Html.FROM_HTML_MODE_COMPACT)
+        } else {
+            tvRules.text = Html.fromHtml(html)
         }
     }
 
