b9411f925ba419049472670186156d81967fc857
sergey <baskervilei08@gmail.com>
2019-07-06 11:53:01 +0400

    load custom avatar work. Without saving

--- app/src/main/java/com/example/sergey/shlypa2/screens/players/PlayersFragment.kt
+++ app/src/main/java/com/example/sergey/shlypa2/screens/players/PlayersFragment.kt
@@ -1,13 +1,10 @@
 package com.example.sergey.shlypa2.screens.players
 
 
-
-import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.view.*
 import android.view.animation.DecelerateInterpolator
@@ -35,6 +32,8 @@ import com.takusemba.spotlight.OnTargetStateChangedListener
 import com.takusemba.spotlight.SimpleTarget
 import com.takusemba.spotlight.Spotlight
 import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImage.ActivityBuilder
+import com.theartofdev.edmodo.cropper.CropImageView
 import eu.davidea.flexibleadapter.FlexibleAdapter
 import kotlinx.android.synthetic.main.fragment_players.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
@@ -51,10 +50,6 @@ class PlayersFragment : androidx.fragment.app.Fragment() {
 
     companion object {
         const val SHOW_SPOTLIGHT = "spotlight_show"
-        const val GALLERY_REQUEST = 10
-        private const val REQUEST_CODE_PERMISSION_READ_IMAGE = 120
-        private const val REQUEST_IMAGE_CAPTURE = 110
-        private const val REQUEST_GET_IMAGE_CAPTURE = 1100
     }
 
     private val borderColor by lazy { Functions.getThemedBgColor(requireContext()) }
@@ -230,57 +225,57 @@ class PlayersFragment : androidx.fragment.app.Fragment() {
     //Load custom avatar
 
     private val getCustomAvatar: (CustomAvatar) -> Unit = { custom ->
-
-        CropImage.activity()
-                .start(requireContext(), this)
-        /* when(custom){
-             CustomAvatar.IMAGE->getImage()
-             CustomAvatar.PHOTO->getPhoto()
-         }*/
+        startCropImageActivity()
     }
 
-    private fun startCropImageActivity(){
-        CropImage.activity()
+    private fun setAvatarCustom(imageUri: Uri) {
+        Glide.with(this)
+                .load(imageUri)
+                .apply(avatarOptions)
+                .into(civPlayerAvatar)
+    }
+
+    private fun startCropImageActivity(imageUri: Uri? = null) {
+        val cropImage: ActivityBuilder = if (imageUri != null) {
+            CropImage.activity(imageUri)
+        } else {
+            CropImage.activity()
+        }
+        cropImage
+                .setCropShape(CropImageView.CropShape.OVAL)
+                .setAspectRatio(150, 150)
+                .setGuidelines(CropImageView.Guidelines.ON)
                 .start(requireContext(), this)
+
+
     }
-    private fun startCropImageActivity(imageUri:Uri){
-        CropImage.activity(imageUri)
-                .start(requireContext(),this)
-    }
+
     private var mCropImageUri: Uri? = null
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (data != null) {
-            if (requestCode == CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-                val imageUri = CropImage.getPickImageResultUri(requireContext(), data)
-                val result =  CropImage.getActivityResult(data).uri
-
-                Glide.with(this)
-                        .load(result)
-                        .apply(avatarOptions)
-                        .into(civPlayerAvatar)
-
-                if (Build.VERSION.SDK_INT >= 23) {
-                    if (CropImage.isReadExternalStoragePermissionsRequired(requireContext(), imageUri)) {
-                        mCropImageUri = imageUri
-                        requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                                CropImage.PICK_IMAGE_PERMISSIONS_REQUEST_CODE)
-                    }else{
-                        startCropImageActivity()
+            when (requestCode) {
+                CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
+                    val result = CropImage.getActivityResult(data)
+                    if (resultCode == Activity.RESULT_OK) {
+                        val resultUri = result.uri
+                        setAvatarCustom(resultUri)
+                    } else {
+                        if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                            Timber.e(result.error)
+                        }
                     }
                 }
-
             }
         }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         if (requestCode == CropImage.PICK_IMAGE_PERMISSIONS_REQUEST_CODE) {
-            if (mCropImageUri != null
-                    && grantResults.isNotEmpty()
+            if (grantResults.isNotEmpty()
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                startCropImageActivity(mCropImageUri!!)
-            }else{
+                startCropImageActivity(mCropImageUri)
+            } else {
                 Toast.makeText(requireContext(),
                         "Cancelling, required permissions are not granted",
                         Toast.LENGTH_LONG)
@@ -288,87 +283,5 @@ class PlayersFragment : androidx.fragment.app.Fragment() {
             }
         }
     }
-/*
-    private fun getImage() {
-        val permissionStatus = ContextCompat.checkSelfPermission(requireContext(),
-                Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
-            loadImage()
-        } else {
-            requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                    REQUEST_CODE_PERMISSION_READ_IMAGE)
-        }
-    }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_PERMISSION_READ_IMAGE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                loadImage()
-            } else {
-
-            }
-        }
-        if (requestCode == REQUEST_GET_IMAGE_CAPTURE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getPhoto()
-            }
-        }
-        return
-    }
-
-    private fun loadImage() {
-        val photoPickerIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        startActivityForResult(photoPickerIntent,
-                GALLERY_REQUEST)
-    }
-
-    private fun getPhoto() {
-        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) ==
-                PackageManager.PERMISSION_GRANTED) {
-            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-                takePictureIntent.resolveActivity(requireActivity().packageManager)?.also {
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-                }
-            }
-        } else {
-            requestPermissions(arrayOf(Manifest.permission.CAMERA), REQUEST_GET_IMAGE_CAPTURE)
-        }
-    }*/
-
-    /* override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-         // super.onActivityResult(requestCode, resultCode, data)
-         if (resultCode == Activity.RESULT_OK && data != null) {
-             when (requestCode) {
-                 GALLERY_REQUEST -> {
-                    *//* CropImage.activity(data.data)
-                            .start(requireContext(), this)*//*
-                }
-
-
-                REQUEST_IMAGE_CAPTURE -> {
-                    *//*  val imageBitmap = data.extras.get("data") as Bitmap
-                      ivWorkoutImage.setImageBitmap(imageBitmap)*//*
-                    //todo do it
-                }
-
-               *//* CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
-                    val imageResultUri = CropImage.getActivityResult(data).uri
-                    val path = imageResultUri.path
-                    val options = RequestOptions()
-                            .centerCrop()
-                    Glide.with(requireContext())
-                            .load(File(path))
-                            .apply(options)
-                            .into(ivWorkoutImage)
-                    ivWorkoutImage.show()
-                    viewModelEdit.setImage(File(path))
-                }*//*
-            }
-        }
-        *//*if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-            //todo i don't know
-            Timber.d("Exception error = result.getError();")
-        }*//*
-    }*/
 }
