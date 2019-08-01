109fa3ac8a75147e008f0629926b94cf7b70e4c5
volgoak <volgoak@gmail.com>
2019-08-02 01:35:14 +0400

    minor fixes

--- app/src/main/java/com/example/sergey/shlypa2/data/PlayersRepository.kt
+++ app/src/main/java/com/example/sergey/shlypa2/data/PlayersRepository.kt
@@ -149,32 +149,18 @@ class PlayersRepository(
     }
 
     private fun createTeams(count: Int) {
-        val shuffledPlayers: MutableList<Player> = players.values.toMutableList()
-        shuffledPlayers.shuffle()
+        val shuffledNames = dataProvider.teamNames.toList().shuffled()
+        val newTeams = (0 until count)
+                .map { Team(shuffledNames.getOrElse(it) {"Team $it"}) }
 
-        val playersQueue = ArrayDeque<Player>()
-        playersQueue.addAll(shuffledPlayers)
+        players.values
+                .shuffled()
+                .forEachIndexed { index, player ->
+            newTeams[index % newTeams.size].players.add(player)
+        }
 
         teams.clear()
-
-        val shuffledTeams = dataProvider.teamNames.toMutableList().shuffled()
-
-
-        for (i in 0 until count) {
-            val teamName = shuffledTeams.getOrElse(i) { "Team $i" }
-            teams.add(Team(teamName))
-        }
-
-        var currentTeam = 0
-        //todo refactor this shit  !!!
-        while (playersQueue.isNotEmpty()) {
-            teams[currentTeam].players.add(playersQueue.remove())
-
-            currentTeam = if (currentTeam >= teams.size - 1) 0 else currentTeam + 1
-        }
-
-
-
+        teams.addAll(newTeams)
         notifyTeams()
     }
 
