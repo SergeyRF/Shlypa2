ce15a913a419f67dc7c475006a8cc22cfa1affa1
sergey <baskervilei08@gmail.com>
2019-08-21 13:34:57 +0400

    delete findViewById

--- app/src/main/java/com/example/sergey/shlypa2/views/SeekbarSetting.kt
+++ app/src/main/java/com/example/sergey/shlypa2/views/SeekbarSetting.kt
@@ -4,9 +4,9 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.SeekBar
-import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.sergey.shlypa2.R
+import kotlinx.android.synthetic.main.setting_seekbar.view.*
 
 /**
  * Created by alex on 4/25/18.
@@ -16,18 +16,15 @@ constructor(context: Context,
             attrs: AttributeSet? = null,
             defStyleAttr: Int = 0) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-    private var title: TextView
-    private lateinit var count: TextView
-    private var seekbar: SeekBar
 
-   private var seekbarListener: ((seekBar: SeekBar?, progress: Int, fromUser: Boolean) -> Unit)? = null
+    private var seekbarListener: ((seekBar: SeekBar?, progress: Int, fromUser: Boolean) -> Unit)? = null
 
     private var minValue = 0
     private var maxValue = 0
 
     private val listener = object : SeekBar.OnSeekBarChangeListener {
         override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-            count.text = (progress + minValue).toString()
+            tvCountSetting.text = (progress + minValue).toString()
             if (fromUser) seekbarListener?.invoke(seekBar, progress + minValue, fromUser)
         }
 
@@ -43,28 +40,24 @@ constructor(context: Context,
     init {
         LayoutInflater.from(context).inflate(R.layout.setting_seekbar, this, true)
 
-        title = findViewById(R.id.tvTitleSwitch)
-        count = findViewById(R.id.tvCountSetting)
-        seekbar = findViewById(R.id.sbSetting)
-
         attrs?.let {
             val array = context.obtainStyledAttributes(it, R.styleable.SeekbarSetting)
-            title.text = array.getText(R.styleable.SeekbarSetting_seek_setting_title)
+            tvTitleSwitch.text = array.getText(R.styleable.SeekbarSetting_seek_setting_title)
             array.recycle()
         }
 
-        seekbar.setOnSeekBarChangeListener(listener)
+        sbSetting.setOnSeekBarChangeListener(listener)
     }
 
     fun setValues(min: Int, max: Int): SeekbarSetting {
         minValue = min
         maxValue = max
-        seekbar.max = maxValue - minValue
+        sbSetting.max = maxValue - minValue
         return this
     }
 
     fun setProgress(progress: Int): SeekbarSetting {
-        seekbar.progress = progress - minValue
+        sbSetting.progress = progress - minValue
         return this
     }
 
@@ -75,8 +68,8 @@ constructor(context: Context,
         return this
     }
 
-    fun setTitle(title:String):SeekbarSetting{
-        this.title.text = title
+    fun setTitle(title: String): SeekbarSetting {
+        tvTitleSwitch.text = title
         return this
     }
 
--- app/src/main/java/com/example/sergey/shlypa2/views/SwitchSetting.kt
+++ app/src/main/java/com/example/sergey/shlypa2/views/SwitchSetting.kt
@@ -4,10 +4,9 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.CompoundButton
-import android.widget.Switch
-import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.sergey.shlypa2.R
+import kotlinx.android.synthetic.main.switch_setting.view.*
 
 /**
  * Created by alex on 4/25/18.
@@ -17,41 +16,36 @@ constructor(context: Context,
             attrs: AttributeSet? = null,
             defStyleAttr: Int = 0) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-    private var title: TextView
-    private var switch: Switch
 
     init {
         LayoutInflater.from(context).inflate(R.layout.switch_setting, this, true)
 
-        title = findViewById(R.id.tvTitleSwitch)
-        switch = findViewById(R.id.switchSetting)
-
         attrs?.let {
             val array = context.obtainStyledAttributes(it, R.styleable.SwitchSetting)
 
-            title.text = array.getText(R.styleable.SwitchSetting_switch_setting_title)
+            tvTitleSwitch.text = array.getText(R.styleable.SwitchSetting_switch_setting_title)
         }
         setOnClickListener {
             setChecked(!isChecked())
         }
     }
 
-    fun isChecked(): Boolean = switch.isChecked
+    fun isChecked(): Boolean = switchSetting.isChecked
 
     fun setChecked(checked: Boolean): SwitchSetting {
-        switch.isChecked = checked
+        switchSetting.isChecked = checked
         return this
     }
 
     fun setOnCheckedListener(listener: (Boolean) -> Unit): SwitchSetting {
-        switch.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { _, isChecked ->
+        switchSetting.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { _, isChecked ->
             listener.invoke(isChecked)
         })
         return this
     }
 
     fun setTitle(title: String): SwitchSetting {
-        this.title.text = title
+        tvTitleSwitch.text = title
         return this
     }
 
